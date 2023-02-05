d63593b95d129e4cb4ca23bccb96903adb9c6102
Christopher Dowdy <cdowdy81@gmail.com>
2023-02-04 21:41:19 -0500

    updated api call. #2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -54,13 +55,15 @@ func init() {
 	recipes = make([]Recipe, 0)
 }
 func NewRecipeHandler(c *gin.Context) {
+	// take in desired ingredients from user and store in variable
 	var ingredients Ingredients
 	if err := c.BindJSON(&ingredients); err != nil {
 		return
 	}
-	//ingredientsString := strings.Join(ingredients.IngredientList, "%2c")
-	//url := "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + ingredientsString + "&number=1&ignorePantry=true&ranking=1"
-	url := "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=5&ignorePantry=true&ranking=1"
+	// convert array of ingredients to string so that it can be in proper format for the url for api call
+	ingredientsString := strings.Join(ingredients.IngredientList, "%2c")
+	url := "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + ingredientsString + "&number=1&ignorePantry=true&ranking=1"
+	// make api GET request to spoonacular API to search for recipes by ingredients
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("X-RapidAPI-Key", "0e2d3a4b52msh4f7ca3d8295bc0ap1374f1jsnaa5308ae1f95")
 	req.Header.Add("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com")
@@ -68,10 +71,13 @@ func NewRecipeHandler(c *gin.Context) {
 	if res.Body != nil {
 		defer res.Body.Close()
 	}
+	//body stores the result of the API call
 	body, _ := ioutil.ReadAll(res.Body)
+	//create a new recipe struct, then put the result of the API call into recipe
 	var recipe Recipe
 	_ = json.Unmarshal(body, &recipe)
 	recipes = append(recipes, recipe)
+	//print new recipe struct that contains the recipe corresponding to the input ingredients
 	c.JSON(http.StatusOK, recipe)
 }
 
@@ -84,5 +90,4 @@ func main() {
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
-
 }
