bffc78ac93c643c799db6dfbbb0fe03d4d84e7d3
Jaden Lindner <jadenlindner@ufl.edu>
2023-02-26 20:42:45 -0500

    tests added for auth and post #9 #10
    post and successful refresh tests don't work (comments above each test explain the issue)

--- main_test.go
+++ main_test.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	
+
 	"foodcraft/models"
+
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -30,5 +32,130 @@ func TestListRecipesHandler(t *testing.T) {
 
 	var recipes []models.Recipe
 	json.Unmarshal(data, &recipes)
-	assert.Equal(t, len(recipes), 21) // check that there are 20 recipes in the database
+	assert.Equal(t, len(recipes), 24) // check that there are 24 recipes in the database
+}
+
+// not working - need authorization + index out of range when no auth problem
+func TestNewRecipeHandler(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	recipe := models.Recipe{
+		ID: 123456,
+	}
+
+	raw, _ := json.Marshal(recipe)
+	resp, err := http.Post(fmt.Sprintf("%s/recipes", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode) // check that the status code is 200
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	var payload map[string]string
+	json.Unmarshal(data, &payload)
+
+	assert.Equal(t, payload["message"], "")
+}
+
+func TestSignInHandler(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "admin",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/signin", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode) // check that the status code is 200
+}
+
+func TestSignInHandlerFail(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "abcd",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/signin", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode) // check that the status code is 401 (invalid user/password)
+}
+
+func TestSignUpHandlerFail(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "admin",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/signup", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode) // check that the status code is 500 (user exists already)
+}
+
+func TestSignUpHandler(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "admin10",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/signup", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusAccepted, resp.StatusCode) // check that the status code is 200
+	data, _ := ioutil.ReadAll(resp.Body)
+
+	var payload map[string]string
+	json.Unmarshal(data, &payload)
+
+	assert.Equal(t, payload["message"], "Account has been created")
+}
+
+func TestRefreshHandlerUnauthorized(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "admin",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/refresh", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusUnauthorized, resp.StatusCode) // check that the status code is 401 (no sign in yet)
+}
+
+// not working - need authorization beforehand
+func TestRefreshHandler(t *testing.T) {
+	ts := httptest.NewServer(SetupServer())
+	defer ts.Close()
+
+	user := models.User{
+		Username: "admin",
+		Password: "password",
+	}
+
+	raw, _ := json.Marshal(user)
+	resp, err := http.Post(fmt.Sprintf("%s/refresh", ts.URL), "application/json", bytes.NewBuffer(raw))
+	defer resp.Body.Close()
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusAccepted, resp.StatusCode) // check that the status code is 200
 }
