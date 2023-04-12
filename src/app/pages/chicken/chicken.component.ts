4fff4a784595b787b6565632665cba7199db6844
davidnoq <davidnoq15@gmail.com>
2023-04-11 21:13:56 -0400

    updated chicken

--- src/app/pages/chicken/chicken.component.ts
+++ src/app/pages/chicken/chicken.component.ts
@@ -1,25 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// creating item for recipes and declared variables
+interface Recipes {
+  title: string;
+  image: string;
+
+}
+
+interface Ingredient {
+  name: string;
+  
+}
+
+
 @Component({
   selector: 'app-chicken',
   templateUrl: './chicken.component.html',
   styleUrls: ['./chicken.component.css']
 })
 export class ChickenComponent implements OnInit {
-  chickenData: any[] = []; // Define a variable to hold the chicken data
-
+  
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchChicken(); // Call fetchChicken() method when the component initializes
   }
 
+  ingredient: Ingredient[] = [{ name: 'Chicken'}]
+  recipes: string[] = [];
+
+  apiUrl = 'http://localhost:8080/api/recipes';
+ 
+
+// backend requests
   fetchChicken() {
-    // Make an HTTP GET request to your backend API endpoint to fetch chicken data
-    this.http.get<any[]>('/recipes').subscribe(
+    
+    const url = `${this.apiUrl}?ingredients=${this.ingredient.join(',').toLowerCase()}`;
+    this.http.post<any>(this.apiUrl, this.ingredient).subscribe(
       (response: any) => {
-        this.chickenData = response; // Assign the fetched data to the chickenData variable
+        this.recipes = response; 
+        console.log(this.recipes);
       },
       (error: any) => {
         console.error('Error fetching chicken data:', error);
