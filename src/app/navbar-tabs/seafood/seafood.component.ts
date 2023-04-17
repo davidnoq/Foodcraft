c8c40ccd3552a80306fb5a6ef9c7c9018112cc84
davidnoq <davidnoq15@gmail.com>
2023-04-16 22:16:01 -0400

    ingredients

--- src/app/navbar-tabs/seafood/seafood.component.ts
+++ src/app/navbar-tabs/seafood/seafood.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// Creating interfaces for recipes and ingredients
+interface Recipe {
+  title: string;
+  image: string;
+}
+
+interface Ingredient {
+  ingredientlist: string;
+}
 
 @Component({
-  selector: 'app-seafood',
+  selector: 'app-pork',
   templateUrl: './seafood.component.html',
   styleUrls: ['./seafood.component.css']
 })
-export class SeafoodComponent {
+export class SeafoodComponent implements OnInit {
+  
+  constructor(private http: HttpClient) { }
 
+  //call fetchChicken() method when the component initializes
+  ngOnInit() {
+    this.fetchSeafood(); 
+  }
+
+  //update to single object instead of an array
+  ingredientlist = ["Salmon"]; 
+  title: string = " ";
+  image: string = " ";
+
+
+  apiUrl = 'http://localhost:8080/api/recipes';
+  
+  //backend requests
+  fetchSeafood() {
+    const data = {
+      ingredientlist: this.ingredientlist
+    }
+    this.http.post(this.apiUrl, data).subscribe(
+      (response: any) => {
+        this.title = response.Title;
+        this.image = response.Image;
+        console.log(response);
+      },
+      (error: any) => {
+        console.error('Error fetching seafood data:', error);
+        
+      }
+    );
+  }
 }
