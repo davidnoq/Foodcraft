20372ab9e363c5bbe0973639e27b2e8a09872381
davidnoq <davidnoq15@gmail.com>
2023-04-18 04:32:10 -0400

    integrated user accounts and instructions button with all components

--- src/app/navbar-tabs/beef/beef.component.ts
+++ src/app/navbar-tabs/beef/beef.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { RecipeDialogComponent } from 'app/recipe-dialog/recipe-dialog.component';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 // Creating interfaces for recipes and ingredients
 interface Recipe {
-  title: string;
-  image: string;
+  ID: number;
+  Title: string;
+  Image: string;
+  Likes: number;
+}
+interface recipeClick {
+  ID: number;
+  Title: string;
+  Image: string;
+  Likes: number;
+  instructions: string;
 }
 
-interface Ingredient {
-  ingredientlist: string;
-}
 
 @Component({
   selector: 'app-beef',
@@ -18,19 +25,24 @@ interface Ingredient {
 })
 export class BeefComponent implements OnInit {
   
-  constructor(private http: HttpClient) { }
-
-  //call fetchChicken() method when the component initializes
-  ngOnInit() {
+  accountData: any;
+    isDetailsDialogOpen: boolean = false;
+  constructor(private http: HttpClient,private dialog: MatDialog) {
+    this.recipeClick = {
+      ID: 0,
+      Title: "",
+      Image: "",
+      Likes: 0,
+      instructions: ""
+  };
+   }
+   ngOnInit() {
     this.fetchBeef(); 
   }
-
   //update to single object instead of an array
-  ingredientlist = ["Beef"]; 
-  title: string = " ";
-  image: string = " ";
-  likes: string = " ";
-
+  ingredientlist = ["ground beef"]; 
+  ingredientArray: Recipe[] = [];
+  recipeClick: recipeClick;
 
   apiUrl = 'http://localhost:8080/api/recipes';
   
@@ -39,11 +51,9 @@ export class BeefComponent implements OnInit {
     const data = {
       ingredientlist: this.ingredientlist
     }
-    this.http.post(this.apiUrl, data).subscribe(
-      (response: any) => {
-        this.title = response.Title;
-        this.image = response.Image;
-        this.likes = response.Likes
+    this.http.post<Recipe[]>(this.apiUrl, data).subscribe(
+      (response: Recipe[]) => {
+        this.ingredientArray = response;
         console.log(response);
       },
       (error: any) => {
@@ -52,4 +62,58 @@ export class BeefComponent implements OnInit {
       }
     );
   }
+
+
+  openDetails(): void {
+    if (!this.isDetailsDialogOpen) { // check if the details dialog is already open
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.data = this.recipeClick; // Pass the recipe data to the dialog component
+
+      const dialogRef = this.dialog.open(RecipeDialogComponent, dialogConfig);
+
+      dialogRef.afterClosed().subscribe(() => {
+        this.isDetailsDialogOpen = false; // set the flag to false when the dialog is closed
+      });
+
+      this.isDetailsDialogOpen = true; // set the flag to true when the dialog is opened
+    }
+}
+
+onRecipeCardClick(recipe: Recipe): void {
+    if (!this.isDetailsDialogOpen) { // check if the details dialog is already open
+        const dialogConfig = new MatDialogConfig();
+        recipeClicked: this.recipeClick;
+
+        this.recipeClick.Title = recipe.Title;
+        this.recipeClick.Likes = recipe.Likes;
+        this.recipeClick.Image = recipe.Image;
+        this.recipeClick.ID = recipe.ID;
+        this.getInstructions();
+
+        dialogConfig.data = this.recipeClick; // Pass the recipe data to the dialog component
+  
+        const dialogRef = this.dialog.open(RecipeDialogComponent, dialogConfig);
+  
+        dialogRef.afterClosed().subscribe(() => {
+          this.isDetailsDialogOpen = false; // set the flag to false when the dialog is closed
+        });
+  
+        this.isDetailsDialogOpen = true; // set the flag to true when the dialog is opened
+      }
+}
+
+getInstructions() {
+  this.http.get('http://localhost:8080/api/recipes/' + this.recipeClick.ID + '/instructions').subscribe(
+    (res: any) => {      
+      this.recipeClick.instructions = res.instructions;
+    }
+  )
+}
+
+removeRecipe(ID: number) {
+  this.http.delete('http://localhost:8080/api/recipes/' + ID).subscribe(
+  (res: any) => {
+      window.location.reload();
+  })
+}
 }
--- src/app/navbar-tabs/chicken/chicken.component.ts
+++ src/app/navbar-tabs/chicken/chicken.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { RecipeDialogComponent } from 'app/recipe-dialog/recipe-dialog.component';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 // Creating interfaces for recipes and ingredients
 interface Recipe {
-  title: string;
-  image: string;
+  ID: number;
+  Title: string;
+  Image: string;
+  Likes: number;
 }
-
-interface Ingredient {
-  ingredientlist: string;
+interface recipeClick {
+  ID: number;
+  Title: string;
+  Image: string;
+  Likes: number;
+  instructions: string;
 }
 
 @Component({
@@ -17,8 +23,17 @@ interface Ingredient {
   styleUrls: ['./chicken.component.css']
 })
 export class ChickenComponent implements OnInit {
-  
-  constructor(private http: HttpClient) { }
+  accountData: any;
+    isDetailsDialogOpen: boolean = false;
+  constructor(private http: HttpClient,private dialog: MatDialog) {
+    this.recipeClick = {
+      ID: 0,
+      Title: "",
+      Image: "",
+      Likes: 0,
+      instructions: ""
+  };
+   }
 
   //call fetchChicken() method when the component initializes
   ngOnInit() {
@@ -26,10 +41,9 @@ export class ChickenComponent implements OnInit {
   }
 
   //update to single object instead of an array
-  ingredientlist = ["Chicken"]; 
-  title: string = " ";
-  image: string = " ";
-  likes: string = " ";
+  ingredientlist = ["chicken"]; 
+  ingredientArray: Recipe[] = [];
+  recipeClick: recipeClick;
 
 
   apiUrl = 'http://localhost:8080/api/recipes';
@@ -39,12 +53,12 @@ export class ChickenComponent implements OnInit {
     const data = {
       ingredientlist: this.ingredientlist
     }
-    this.http.post(this.apiUrl, data).subscribe(
-      (response: any) => {
-        this.title = response.Title;
-        this.image = response.Image;
-        this.likes = response.Likes
-        console.log(response);
+    this.http.post<Recipe[]>(this.apiUrl, data).subscribe(
+      (response: Recipe[]) => {
+        this.ingredientArray = response;
+        
+        console.log(this.ingredientArray);
+        
       },
       (error: any) => {
         console.error('Error fetching chicken data:', error);
@@ -52,4 +66,57 @@ export class ChickenComponent implements OnInit {
       }
     );
   }
+
+  openDetails(): void {
+    if (!this.isDetailsDialogOpen) { // check if the details dialog is already open
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.data = this.recipeClick; // Pass the recipe data to the dialog component
+
+      const dialogRef = this.dialog.open(RecipeDialogComponent, dialogConfig);
+
+      dialogRef.afterClosed().subscribe(() => {
+        this.isDetailsDialogOpen = false; // set the flag to false when the dialog is closed
+      });
+
+      this.isDetailsDialogOpen = true; // set the flag to true when the dialog is opened
+    }
+}
+
+onRecipeCardClick(recipe: Recipe): void {
+    if (!this.isDetailsDialogOpen) { // check if the details dialog is already open
+        const dialogConfig = new MatDialogConfig();
+        recipeClicked: this.recipeClick;
+
+        this.recipeClick.Title = recipe.Title;
+        this.recipeClick.Likes = recipe.Likes;
+        this.recipeClick.Image = recipe.Image;
+        this.recipeClick.ID = recipe.ID;
+        this.getInstructions();
+
+        dialogConfig.data = this.recipeClick; // Pass the recipe data to the dialog component
+  
+        const dialogRef = this.dialog.open(RecipeDialogComponent, dialogConfig);
+  
+        dialogRef.afterClosed().subscribe(() => {
+          this.isDetailsDialogOpen = false; // set the flag to false when the dialog is closed
+        });
+  
+        this.isDetailsDialogOpen = true; // set the flag to true when the dialog is opened
+      }
+}
+
+getInstructions() {
+  this.http.get('http://localhost:8080/api/recipes/' + this.recipeClick.ID + '/instructions').subscribe(
+    (res: any) => {      
+      this.recipeClick.instructions = res.instructions;
+    }
+  )
+}
+
+removeRecipe(ID: number) {
+  this.http.delete('http://localhost:8080/api/recipes/' + ID).subscribe(
+  (res: any) => {
+      window.location.reload();
+  })
+}
 }
